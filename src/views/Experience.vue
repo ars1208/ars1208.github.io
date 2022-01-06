3f287a1b1935d4307b262badf14b46209103e06a
ars1208 <ara.kaz1208@gmail.com>
2022-01-06 21:41:57 +0900

    add experience page

---
+++ src/views/Experience.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Experience</h1>
+    <p>sample info</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Experience",
+
+  components: {},
+});
+</script>
