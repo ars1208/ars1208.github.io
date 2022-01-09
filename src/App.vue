f3b044fa46b4037112b69ffba39513627da857b5
ars1208 <ara.kaz1208@gmail.com>
2022-01-09 21:11:42 +0900

    code format

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <template>
   <v-app>
-    <v-container fluid class="ma-0 pa-0">
-      <v-row>
-        <v-col cols="3">
-          <side-navigation></side-navigation>
-        </v-col>
-        <v-col>
-          <v-main>
-            <router-view />
-          </v-main>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-main>
+      <router-view />
+    </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import SideNavigation from "@/components/SideNavigation.vue";
 
 export default Vue.extend({
   name: "App",
 
-  components: {
-    SideNavigation
-  },
+  components: {},
 });
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,43 @@
 <template>
   <div>
-    <about></about>
-    <experience></experience>
-    <education></education>
-    <skills></skills>
-    <interests></interests>
+    <v-container fluid class="ma-0 pa-0">
+      <v-row>
+        <v-col cols="3">
+          <side-navigation></side-navigation>
+        </v-col>
+        <v-col>
+          <div>
+            <about></about>
+            <experience></experience>
+            <education></education>
+            <skills></skills>
+            <interests></interests>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import About from "@/views/About.vue"
-import Experience from "@/views/Experience.vue"
-import Education from "@/views/Education.vue"
-import Skills from "@/views/Skills.vue"
-import Interests from "@/views/Interests.vue"
+import About from "@/views/About.vue";
+import Experience from "@/views/Experience.vue";
+import Education from "@/views/Education.vue";
+import Skills from "@/views/Skills.vue";
+import Interests from "@/views/Interests.vue";
+import SideNavigation from "@/components/SideNavigation.vue";
 
 export default Vue.extend({
   name: "Home",
 
   components: {
+    SideNavigation,
     About,
     Experience,
     Education,
     Skills,
-    Interests
+    Interests,
   },
 });
 </script>
