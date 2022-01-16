a848fbd4a722bcd8d3b43f83a65a8db48564dfce
ars1208 <ara.kaz1208@gmail.com>
2022-01-16 22:21:45 +0900

    fix design

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,30 +1,34 @@
 <template>
   <div class="skills__main">
     <h1>Skills</h1>
-    <div class="skills__category">
-      <h2>Programing</h2>
-      <ul>
-        <li>Python3</li>
-        <li>TypeScript</li>
-        <li>Vue.js</li>
-        <li>Java</li>
-      </ul>
-    </div>
-    <div class="skills__category">
-      <h2>Language</h2>
-      <ul>
-        <li>Japanese</li>
-        <li>English</li>
-      </ul>
-    </div>
-    <div class="skills__category">
-      <h2>Certification</h2>
-      <ul>
-        <li>基本情報技術者</li>
-        <li>応用情報技術者</li>
-        <li>Microsoft Azure Fundamentals (AZ-900)</li>
-      </ul>
-    </div>
+    <v-container>
+      <v-row>
+        <v-col class="skills__category">
+          <h2>Programing</h2>
+          <ul>
+            <li>Python3</li>
+            <li>TypeScript</li>
+            <li>Vue.js</li>
+            <li>Java</li>
+          </ul>
+        </v-col>
+        <v-col class="skills__category">
+          <h2>Language</h2>
+          <ul>
+            <li>Japanese</li>
+            <li>English</li>
+          </ul>
+        </v-col>
+        <v-col class="skills__category">
+          <h2>Certification</h2>
+          <ul>
+            <li>基本情報技術者</li>
+            <li>応用情報技術者</li>
+            <li>Microsoft Azure Fundamentals (AZ-900)</li>
+          </ul>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
