4a3b5c509af4d7914f7e3b3f0492399ad7bc4d15
ars1208 <ara.kaz1208@gmail.com>
2022-01-17 19:38:09 +0900

    fix vuetify component

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -3,29 +3,35 @@
     <h1>Skills</h1>
     <v-container>
       <v-row>
-        <v-col class="skills__category">
-          <h2>Programing</h2>
-          <ul>
-            <li>Python3</li>
-            <li>TypeScript</li>
-            <li>Vue.js</li>
-            <li>Java</li>
-          </ul>
+        <v-col>
+          <v-card height="300px">
+            <h2>Programing</h2>
+            <ul>
+              <li>Python3</li>
+              <li>TypeScript</li>
+              <li>Vue.js</li>
+              <li>Java</li>
+            </ul>
+          </v-card>
         </v-col>
-        <v-col class="skills__category">
-          <h2>Language</h2>
-          <ul>
-            <li>Japanese</li>
-            <li>English</li>
-          </ul>
+        <v-col>
+          <v-card height="300px">
+            <h2>Language</h2>
+            <ul>
+              <li>Japanese</li>
+              <li>English</li>
+            </ul>
+          </v-card>
         </v-col>
-        <v-col class="skills__category">
-          <h2>Certification</h2>
-          <ul>
-            <li>基本情報技術者</li>
-            <li>応用情報技術者</li>
-            <li>Microsoft Azure Fundamentals (AZ-900)</li>
-          </ul>
+        <v-col>
+          <v-card height="300px">
+            <h2>Certification</h2>
+            <ul>
+              <li>基本情報技術者</li>
+              <li>応用情報技術者</li>
+              <li>Microsoft Azure Fundamentals (AZ-900)</li>
+            </ul>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
