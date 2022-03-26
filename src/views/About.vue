bdbcdccc02ecb1372a09f1ba2bdc66f3cf9baddc
ars1208 <ara.kaz1208@gmail.com>
2022-03-26 23:55:53 +0900

    add info

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,8 @@
   <div id="about" class="about__main">
     <h1>KAZUKI ARASHI</h1>
     <ul>
-      <li>GitHub</li>
+      <li><div class="about__info_icon-bg"><img src="../assets/github-brands.svg" alt="GitHub" class="about__info_icon" /></div></li>
+      <li><div class="about__info_icon-bg"><img src="../assets/linkedin-in-brands.svg" alt="Linketin" class="about__info_icon" /></div></li>
       <li>AtCoder</li>
       <li>Gmail</li>
     </ul>
@@ -23,5 +24,18 @@ export default Vue.extend({
 <style scoped>
 .about__main {
   height: 100vh;
+}
+.about__info_icon-bg {
+  height: 50px;
+  width: 50px;
+  background-color: #5b67a2;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+}
+.about__info_icon {
+  height: 30px;
+  display: block;
+  margin: auto;
 }
 </style>
