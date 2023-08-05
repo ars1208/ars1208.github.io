bd046b4eb75ac91f323288094c75155394b554e3
ars1208 <ara.kaz1208@gmail.com>
2023-08-05 17:11:34 +0900

    feat: change react

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          ars1208 page
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
